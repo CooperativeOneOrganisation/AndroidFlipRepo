8632636fbd0e23563ba08fdbd4b18334ffe3cbb5
Yasir R <yasircode@gmail.com>
2013-03-12 16:09:47 +1100

    Commiting Changes Version 1.1
    Added layout with simple button and Flip Coin Logic

--- AndroidFlip/src/com/coopone/androidflip/MainActivity.java
+++ AndroidFlip/src/com/coopone/androidflip/MainActivity.java
@@ -1,15 +1,23 @@
 package com.coopone.androidflip;
 
-import android.os.Bundle;
+import java.util.Random;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	TextView flipResult;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		flipResult = (TextView) findViewById(R.id.FlipResult);
 	}
 
 	@Override
@@ -19,4 +27,21 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public void flipCoin(View view)
+	{
+		int randomNumber;
+		System.out.println("Flip Coin Button Clicked");
+		Random random = new Random();
+		randomNumber = random.nextInt();
+		System.out.println("Random Number :" +randomNumber);
+		
+		if ((randomNumber % 2) == 0)
+		{
+			flipResult.setText(R.string.heads);
+		}
+		else
+		{
+			flipResult.setText(R.string.tails);
+		}
+	}
 }
